dc7621b1d4e4d4f9d0caa594b73635032f9eb5c9
ashkan semsar <ashkansemsar@hotmail.com>
2025-04-02 18:10:44 +0400

    clear selected item

--- MicrosoftNote/Views/AllNotesPage.xaml.cs
+++ MicrosoftNote/Views/AllNotesPage.xaml.cs
@@ -1,12 +1,18 @@
+using MicrosoftNote.ViewModels;
+
 namespace MicrosoftNote.Views;
 public partial class AllNotesPage : ContentPage
 {
     public AllNotesPage()
     {
         InitializeComponent();
-
-       
     }
 
-
+    protected override void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        if (BindingContext is NotesViewModel model)
+        {
+            notesCollection.SelectedItem = null;
+        }
+    }
 }
